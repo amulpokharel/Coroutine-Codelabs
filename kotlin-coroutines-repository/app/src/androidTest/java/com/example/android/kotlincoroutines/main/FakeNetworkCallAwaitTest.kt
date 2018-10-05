c2d06ac0f80eaf8a1a36d13d7e8b59e435c6ad46
Amul Pokharel <amulpokh@gmail.com>
2018-10-05 11:54:00 -0400

    added test for coroutine

--- kotlin-coroutines-repository/app/src/androidTest/java/com/example/android/kotlincoroutines/main/FakeNetworkCallAwaitTest.kt
+++ kotlin-coroutines-repository/app/src/androidTest/java/com/example/android/kotlincoroutines/main/FakeNetworkCallAwaitTest.kt
@@ -3,6 +3,8 @@ package com.example.android.kotlincoroutines.main
 import com.example.android.kotlincoroutines.main.fakes.makeFailureCall
 import com.example.android.kotlincoroutines.main.fakes.makeSuccessCall
 import com.example.android.kotlincoroutines.util.FakeNetworkException
+import com.google.common.truth.Truth
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -20,7 +22,8 @@ class FakeNetworkCallAwaitTest {
     @Test(expected = FakeNetworkException::class)
     fun whenFakeNetworkCallFailure_throws() {
         val subject = makeFailureCall(FakeNetworkException("the error"))
-
-        // TODO: Implement test for await failure
+        runBlocking {
+            Truth.assertThat(subject.await()).isEqualTo("the title")
+        }
     }
 }
